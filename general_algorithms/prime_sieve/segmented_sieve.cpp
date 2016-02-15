ea9cdce930f09b3ec608fdd397b85ccc94bde200
grantmwilliams <grant.williams@okstate.edu>
2016-02-14 19:31:12 -0600

    added segmented sieve to general algorithms

---
+++ general_algorithms/prime_sieve/segmented_sieve.cpp
@@ -0,0 +1,81 @@
+//Segmented Prime Number Function
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include <stdint.h>
+
+/// Set your CPU's L1 data cache size (in bytes) here
+const int L1D_CACHE_SIZE = 32768;
+
+/// Generate primes using the segmented sieve of Eratosthenes.
+/// This algorithm uses O(n log log n) operations and O(sqrt(n)) space.
+/// @param limit         Sieve primes <= limit.
+/// @param segment_size  Size of the sieve array in bytes.
+///
+void segmented_sieve(int64_t limit, const int segment_size)
+{
+  int sqrt = (int) std::sqrt((double) limit);
+  int64_t count = (limit < 2) ? 0 : 1;
+  int64_t s = 2;
+  int64_t n = 3;
+
+  // vector used for sieving
+  std::vector<char> sieve(segment_size);
+
+  // generate small primes <= sqrt
+  std::vector<char> is_prime(sqrt + 1, 1);
+  for (int i = 2; i * i <= sqrt; i++)
+    if (is_prime[i])
+      for (int j = i * i; j <= sqrt; j += i)
+        is_prime[j] = 0;
+
+  std::vector<int> primes;
+  std::vector<int> next;
+
+  for (int64_t low = 0; low <= limit; low += segment_size)
+  {
+    std::fill(sieve.begin(), sieve.end(), 1);
+
+    // current segment = interval [low, high]
+    int64_t high = std::min(low + segment_size - 1, limit);
+
+    // store small primes needed to cross off multiples
+    for (; s * s <= high; s++)
+    {
+      if (is_prime[s])
+      {
+        primes.push_back((int) s);
+          next.push_back((int)(s * s - low));
+      }
+    }
+    // sieve the current segment
+    for (std::size_t i = 1; i < primes.size(); i++)
+    {
+      int j = next[i];
+      for (int k = primes[i] * 2; j < segment_size; j += k)
+        sieve[j] = 0;
+      next[i] = j - segment_size;
+    }
+
+    for (; n <= high; n += 2)
+      if (sieve[n - low]) // n is a prime
+        count++;
+  }
+
+  std::cout << count << " primes found." << std::endl;
+}
+
+int main(int argc, char *argv[]) 
+{
+    
+	int64_t num_primes = 100000000;
+
+	const int segment_size = L1D_CACHE_SIZE;
+
+	segmented_sieve(num_primes,segment_size);
+
+
+    return 0;
+}
